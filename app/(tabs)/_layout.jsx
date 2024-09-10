e4c132c5d80c13b1a5c9bc4af4592b550fe0a744
jevs7714 <jevecsbarnics@gmail.com>
2024-09-10 21:04:27 +0800

    first commit

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,108 @@
+import { View, Text, Image } from 'react-native'
+import { Tabs, Redirect } from 'expo-router'
+import { icons } from '../../constants'
+
+const TabIcon = ({ icon, color, name, focused}) => {
+  return (
+    <View className = "items-center justify-center gap-2">
+      <Image 
+      source={icon}
+      resizeMode= "contain"
+      tintColor={color}
+      className = "w-7 h-7"
+      />
+      <Text className = {`${focused ? 'font-semibold' : 'font-pregular'} text-xs`} style = {{color:color}}>{name}</Text>
+    </View>
+  )
+}
+
+
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs 
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#079604',
+        tabBarStyle: {
+          backgroundColor: '#FFFFFF',
+          borderTopWidth: 1,
+          borderTopColor: '#0085FF',
+          height: 70,
+        }
+      }}>
+       
+
+        <Tabs.Screen 
+        name= "home"
+        options={{
+          title: 'Home',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon 
+              icon = {icons.home}
+              color = {color}
+              name = "Home"
+              focused = {focused}
+            />
+          )
+        }}
+        />
+
+                  
+          <Tabs.Screen 
+                  name= "create"
+                  options={{
+                    title: 'Create',
+                    headerShown: false,
+                    tabBarIcon: ({ color, focused }) => (
+                      <TabIcon 
+                        icon = {icons.plus}
+                        color = {color}
+                        name = "Publish"
+                        focused = {focused}
+                      />
+                    )
+                  }}
+                  />
+
+
+                          
+                <Tabs.Screen 
+                name= "bookmark"
+                options={{
+                  title: 'Bookmark',
+                  headerShown: false,
+                  tabBarIcon: ({ color, focused }) => (
+                    <TabIcon 
+                      icon = {icons.bookmark}
+                      color = {color}
+                      name = "Your Apps"
+                      focused = {focused}
+                    />
+                  )
+                }}
+                />
+
+        
+              <Tabs.Screen 
+                      name= "profile"
+                      options={{
+                        title: 'Profile',
+                        headerShown: false,
+                        tabBarIcon: ({ color, focused }) => (
+                          <TabIcon 
+                            icon = {icons.profile}
+                            color = {color}
+                            name = "Profile"
+                            focused = {focused}
+                          />
+                        )
+                      }}
+                      />
+      </Tabs>
+    </>
+  )
+}
+
+export default TabsLayout
---
+++ app/index.jsx
@@ -0,0 +1,55 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View, Image, ScrollView} from 'react-native';
+import { Redirect, router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
+
+
+export default function App() {
+  return (
+    <SafeAreaView className = "bg-white h-full">
+      <ScrollView contentContainerStyle = {{height:'100%'}}>
+        <View className = "w-full justify-center items-center min-h-[85vh] px-4">
+          <Image 
+            source={images.logoConso}
+            className = "w-[130px] h-[84px] mt-7"
+            resizeMode="contain"
+          />
+
+          <View className = "relative mt-4 ">
+            <Text className = "text-4xl text-black-100 font-bold text-justify">
+              Connect, Share and Inspire with {' '}
+                <Text className = "text-secondary-500">Conso</Text>
+               
+            </Text>
+
+          </View>
+
+
+          <Image 
+            source={images.appsPic}
+            className = "max-2-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+
+          <View className = "relative mb-3 ">
+            <Text className = "text-sm text-gray-700 font-bold text-justify">
+            Where Innovation Meets Simplicity—Unlock a World of Apps Designed to Enhance Every Aspect of Your Digital Life
+            </Text>
+
+          </View>
+
+          <CustomButton 
+            title= "Go to Home"
+            handlePress={() => {router.push('/sign-in')}}
+            containerStyle= "w-full mt-1"
+          />
+        </View>
+
+      </ScrollView>
+      <StatusBar  backgroundColor='#161622' style='light'/>
+    </SafeAreaView>
+  );
+}
+
